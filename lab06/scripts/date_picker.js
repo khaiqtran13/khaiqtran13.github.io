fe6c459f6d86db22674c2028b55733a0ee62a4ab
Khai Tran <khaitran@Khais-MacBook-Air.local>
2021-06-27 17:29:24 -0400

    fixed datepicker for physician select

--- lab06/scripts/date_picker.js
+++ lab06/scripts/date_picker.js
@@ -1,59 +1,55 @@
-function updatePhysician(){
-    console.log(document.getElementById("physician-choice").value);
-}
-
-// reference to http://jsfiddle.net/nicolapeluchetti/dAyzq/1/
+// // reference to http://jsfiddle.net/nicolapeluchetti/dAyzq/1/
 // $(document).ready(function() {     
 // //automatically sets date to current time
 // var dateToday = new Date();
 // console.log(dateToday);
 
-// if (document.getElementById("physician-choice").value === "lilpump" ) {
-//     var dates = $("#my_date_picker").datepicker({
-//         // beforeShowDay: $.datepicker.noWeekends, //disables weekends
-//         defaultDate: "+1w",
-//         changeMonth: true, //allows manual selection of month and year
-//         changeYear: true,
-//         //numberOfMonths: <int>, //number of months to display at once
-//         minDate: dateToday,
-//         onSelect: function(selectedDate) {
-//             var option = this.id == "from" ? "minDate" : "maxDate",
-//                 instance = $(this).data("datepicker"),
-//                 date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
-//             dates.not(this).datepicker("option", option, date);
-//         }
-//     });
-// } else {
-//     var dates = $("#my_date_picker").datepicker({
-//         beforeShowDay: $.datepicker.noWeekends, //disables weekends
-//         defaultDate: "+1w",
-//         changeMonth: true, //allows manual selection of month and year
-//         changeYear: true,
-//         //numberOfMonths: <int>, //number of months to display at once
-//         minDate: dateToday,
-//         onSelect: function(selectedDate) {
-//             var option = this.id == "from" ? "minDate" : "maxDate",
-//                 instance = $(this).data("datepicker"),
-//                 date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
-//             dates.not(this).datepicker("option", option, date);
-//         }
-//     });
-// }
-
+// var dates = $("#my_date_picker").datepicker({
+//     beforeShowDay: $.datepicker.noWeekends, //disables weekends
+//     defaultDate: "+1w",
+//     changeMonth: true, //allows manual selection of month and year
+//     changeYear: true,
+//     //numberOfMonths: <int>, //number of months to display at once
+//     minDate: dateToday,
+//     onSelect: function(selectedDate) {
+//         var option = this.id == "from" ? "minDate" : "maxDate",
+//             instance = $(this).data("datepicker"),
+//             date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
+//         dates.not(this).datepicker("option", option, date);
+//     }
+// });
 // })
+    
 
-function updateDatePicker() {
-    $("#my_date_picker").datepicker("destroy");     
+
+function updateDatePicker(){
+    $( "#my_date_picker" ).datepicker("destroy");
+    
     var dateToday = new Date();
-    // console.log(dateToday);
+    var currPhy = document.getElementById("physician-choice").value;
+    console.log(currPhy);
 
-    var currPhyiscian = document.getElementById("physician-choice").value;
-    console.log("current physician: " + currPhyiscian);
-
-    // lilpump on that grind he works 7 days a week
-    if (currPhyiscian === "lilpump" ) {
+    if (currPhy === "lilpump") {
+        console.log("ESKTITTT");
         var dates = $("#my_date_picker").datepicker({
-            // beforeShowDay: $.datepicker.noWeekends, //disables weekends
+            beforeShowDay: function(date){ return [date.getDay() == 6 || date.getDay() == 0,""]},
+            defaultDate: "+1w",
+            changeMonth: true, //allows manual selection of month and year
+            changeYear: true,
+            //numberOfMonths: <int>, //number of months to display at once
+            minDate: dateToday,
+            onSelect: function(selectedDate) {
+                var option = this.id == "from" ? "minDate" : "maxDate",
+                    instance = $(this).data("datepicker"),
+                    date = $.datepicker.parseDate(instance.settings.dateFormat || $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
+                dates.not(this).datepicker("option", option, date);
+            }
+        });
+    } else if(currPhy === ""){
+        console.log("empty");
+    } else if(currPhy === "ryder-balmes"){
+        var dates = $("#my_date_picker").datepicker({
+            beforeShowDay: function(date){ return [date.getDay() == 2 || date.getDay() == 3,""]},
             defaultDate: "+1w",
             changeMonth: true, //allows manual selection of month and year
             changeYear: true,
@@ -82,9 +78,5 @@ function updateDatePicker() {
             }
         });
     }
-    
-    }
-
-
-
-
+    $( "#datepicker" ).datepicker("refresh");
+}
