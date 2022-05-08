77ef8b1c8c9cb210c1920ca219e5177c6b01dffc
Khai Tran <64976067+khaiqtran13@users.noreply.github.com>
2022-05-08 17:02:29 -0400

    v2 website

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 import TypeAnimation from "react-type-animation";
+import LinkedInIcon from "./linkedin.png";
+import GitHubIcon from "./github.png";
+import resumeIcon from "./resume.png";
 
 export const Home = () => {
   return (
     <div className="h-screen bg-black flex flex-col justify-center">
-      <div className="mx-auto" style={{ width: "420px" }}>
+      <div className="mx-auto" style={{ width: "690px" }}>
         <p className="pb-4 text-6xl">Khai Tran</p>
-        <TypeAnimation
-          cursor={false}
-          sequence={["Third Year Software Engineering Student"]}
-          wrapper="p"
-        />
-        <TypeAnimation
-          cursor={false}
-          sequence={[2500, " at the University of Ottawa"]}
-          wrapper="p"
-        />
+        <div style={{ width: "420px" }}>
+          <TypeAnimation
+            cursor={false}
+            sequence={["Third Year Software Engineering Student"]}
+            wrapper="p"
+          />
+        </div>
+        <div style={{ width: "420px" }} className="pb-4">
+          <TypeAnimation
+            cursor={false}
+            sequence={[2500, " at the University of Ottawa"]}
+            wrapper="p"
+          />
+        </div>
+        <div className="flex space-x-2">
+          <a href="https://www.linkedin.com/in/khai-tran-9a2799196/" target="">
+            <img src={LinkedInIcon} />
+          </a>
+          <a href="https://github.com/khaiqtran13" target="">
+            <img src={GitHubIcon} />
+          </a>
+          <a href="https://github.com/khaiqtran13/resume" target="">
+            <img src={resumeIcon} />
+          </a>
+        </div>
       </div>
     </div>
   );
