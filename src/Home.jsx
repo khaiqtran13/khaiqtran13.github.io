2ff810b2314a415453d8d47f060a57640450b83a
Khai Tran <khaiqtran13@gmail.com>
2022-05-10 22:00:48 -0400

    UI fixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,62 +4,70 @@ import LinkedInIcon from "./linkedin.png";
 import GitHubIcon from "./github.png";
 import resumeIcon from "./resume.png";
 import styled, { keyframes } from "styled-components";
-import { bounce, tada } from "react-animations";
+import { bounce, flip } from "react-animations";
 import ArrowDown from "./arrow-down.png";
 export const Home = () => {
-  const bounceAnimation = keyframes`${bounce}`;
-  const pulseAnimation = keyframes`${tada}`;
+    const bounceAnimation = keyframes`${bounce}`;
+    const animation = keyframes`${flip}`;
 
-  const BouncyDiv = styled.div`
-    animation: 2s ${bounceAnimation};
-    animation-delay: 4s;
-  `;
+    const BouncyDiv = styled.div`
+        animation: 2s ${bounceAnimation};
+        animation-delay: 4s;
+    `;
 
-  const PulseDiv = styled.div`
-    animation: 3s ${pulseAnimation};
-    animation-iteration-count: infinite;
-    animation-delay: 2s;
-  `;
+    const PulseDiv = styled.div`
+        animation: 5s ${animation};
+        animation-iteration-count: infinite;
+        animation-delay: 5s;
+    `;
 
-  return (
-    <div className="flex justify-center flex-col h-screen bg-black">
-      <div className="mx-auto" style={{ width: "690px" }}>
-        <p className="pb-4 text-6xl">Khai Tran</p>
-        <div style={{ width: "420px" }}>
-          <TypeAnimation
-            cursor={false}
-            sequence={["Third Year Software Engineering Student"]}
-            wrapper="p"
-          />
+    return (
+        <div className="relative flex justify-center flex-col h-screen bg-black w-screen">
+            <div className="absolute top-1/3 left-4 md:left-1/4 mx-auto h-16">
+                <p className="pb-4 text-6xl">Khai Tran</p>
+                <div>
+                    <TypeAnimation
+                        cursor={false}
+                        sequence={["Third Year Software Engineering Student"]}
+                        wrapper="p"
+                    />
+                </div>
+                <div className="pb-4">
+                    <TypeAnimation
+                        cursor={false}
+                        sequence={[2500, " at the University of Ottawa"]}
+                        wrapper="p"
+                    />
+                </div>
+                <BouncyDiv className="flex space-x-2">
+                    <a
+                        href="https://www.linkedin.com/in/khai-tran-9a2799196/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={LinkedInIcon} />
+                    </a>
+                    <a
+                        href="https://github.com/khaiqtran13"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={GitHubIcon} />
+                    </a>
+                    <a
+                        href="https://drive.google.com/file/d/1JVSY2eKG4zkF7jIOGt2gpNzNMwIqRooj/view?usp=sharing"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={resumeIcon} alt={"resume"} />
+                    </a>
+                </BouncyDiv>
+            </div>
+            <div className="absolute bottom-0 flex justify-center w-full">
+                <PulseDiv>
+                    <img src={ArrowDown} className="h-16" alt={"arrow down"} />
+                </PulseDiv>
+            </div>
         </div>
-        <div style={{ width: "420px" }} className="pb-4">
-          <TypeAnimation
-            cursor={false}
-            sequence={[2500, " at the University of Ottawa"]}
-            wrapper="p"
-          />
-        </div>
-        <BouncyDiv className="flex space-x-2">
-          <a
-            href="https://www.linkedin.com/in/khai-tran-9a2799196/"
-            target="_blank"
-          >
-            <img src={LinkedInIcon} />
-          </a>
-          <a href="https://github.com/khaiqtran13" target="_blank">
-            <img src={GitHubIcon} />
-          </a>
-          <a
-            href="https://drive.google.com/file/d/1JVSY2eKG4zkF7jIOGt2gpNzNMwIqRooj/view?usp=sharing"
-            target="_blank"
-          >
-            <img src={resumeIcon} />
-          </a>
-        </BouncyDiv>
-      </div>
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0">
-        <img src={ArrowDown} className="" />
-      </div>
-    </div>
-  );
+    );
 };
