548f1bdc422f70d50e2aaa8df1ab131a0cd31464
Khai Tran <64976067+khaiqtran13@users.noreply.github.com>
2022-09-04 13:34:24 -0400

    implement cards

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,89 +1,76 @@
 import React from "react";
-import handleViewport from "react-in-viewport";
-import { useSpring, animated, useTrail } from "react-spring";
+import { FaLink, FaCode } from "react-icons/fa";
+
 export const Projects = () => {
-    const viewportWidth = window.innerWidth;
-
-    // add arrow emoji?
-    const items = ["About Me", "Languages", "Technologies", "Projects"];
-    const trailConfig = { mass: 10, tension: 2000, friction: 200 };
-
-    //
-    const [flip, setFlip] = React.useState(true);
-    const props = useSpring({
-        to: { transform: `translateX(${viewportWidth / 4}px)`, opacity: 1 },
-        from: { transform: "translateX(0px)", opacity: 1 },
-        reset: true,
-        reverse: flip,
-        // delay: 250,
-        // onRest: () => setFlip(!flip),
-    });
-
-    //   block
-    const [toggle, setToggle] = React.useState(false);
-
-    const trail = useTrail(items.length, {
-        trailConfig,
-        opacity: toggle ? 1 : 0,
-        x: toggle ? 0 : 20,
-        from: { opacity: 0, height: -20 },
-    });
-    console.log("vp:", viewportWidth);
-    const TrailingBlock = (props) => {
-        const { forwardedRef } = props;
-        return (
-            <div className="flex" ref={forwardedRef}>
-                <div>
-                    {trail.map(({ x, ...rest }, index) => (
-                        <animated.div
-                            key={items[index]}
-                            className="trails-text"
-                            style={{
-                                ...rest,
-                                transform: x.to(
-                                    (x) => `translate3d(0,${x}px, 0)`,
-                                ),
-                            }}
-                        >
-                            <animated.div className="bg-white rounded-md h-fit p-2 my-4 hover:shadow-xl hover:bg-slate-100 hover:text-indigo-600">
-                                <div
-                                    onClick={() => {
-                                        setFlip(!flip);
-                                    }}
-                                >
-                                    {items[index]}
-                                </div>
-                                {/* onclick shifts everything to the left and shows details on the right */}
-                            </animated.div>
-                        </animated.div>
-                    ))}
-                </div>
-            </div>
-        );
-    };
-
-    const ViewportBlockTrailing = handleViewport(TrailingBlock);
-
-    return (
-        <div className="h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex justify-center">
-            <div className="flex flex-col justify-center">
-                <div className="flex">
-                    {/* {!flip && ( */}
-                    <div className="p-4 bg-white rounded-lg shadow-lg h-96 m-4">
-                        <p className="text-xl text-indigo-500">Title</p>
-                        <p className="text-indigo-500">Title</p>
-                    </div>
-                    {/* )} */}
-                    <div className="w-full">
-                        <animated.div className="overflow-hidden" style={props}>
-                            <ViewportBlockTrailing
-                                onEnterViewport={() => setToggle(true)}
-                                onLeaveViewport={() => setToggle(false)}
-                            />
-                        </animated.div>
-                    </div>
-                </div>
-            </div>
+  const Card = ({ title, subtitle, link, repo, tech }) => (
+    <div className="justify-between glass p-3 m-2 md:w-[690px] h-[210px]">
+      <div className="ml-5 mr-2 grid grid-cols-6 h-full">
+        <div className="flex flex-col col-span-5">
+          <h3 className="mt-2 text-white text-3xl font-semibold">{title}</h3>
+          <p className="mt-1 text-white text-sm text-light">{subtitle}</p>
         </div>
-    );
+        <div className="flex flex-col justify-center h-full">
+          <div className="space-x-2">
+            {link && (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="w-fit hover:scale-150"
+              >
+                <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-150">
+                  <FaLink />
+                </button>
+              </a>
+            )}
+            {repo && (
+              <a
+                href={repo}
+                target="_blank"
+                rel="noreferrer"
+                className="w-fit  hover:scale-125"
+              >
+                <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-150">
+                  <FaCode />
+                </button>
+              </a>
+            )}
+          </div>
+        </div>
+        <div>
+          <p className="text-sm text-light">{tech}</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  return (
+    <div className="h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex justify-center">
+      <div className="flex flex-wrap max-w-3/4 justify-center h-fit">
+        <Card
+          title="CoverMe"
+          tech="Rea"
+          link="https://shift-replacement-finder.herokuapp.com/"
+          subtitle="In a single intuitive mobile app, easily post, cover, and keep track of your shifts.
+        SRF is an app that helps employees find and keep track of replacements.
+        Employees can easily post, cover and browse shifts within your corporation.
+        Admins can view replacements and have access to management privileges."
+        />
+        <Card
+          title="Krypt"
+          link="https://khaiqtran13-krypt.netlify.app/"
+          repo="https://github.com/khaiqtran13/krypt"
+        />
+        <Card
+          title="OnBoardPlus"
+          repo="https://github.com/jakekellett/GNG-2101-Onboard"
+        />
+        <Card
+          title="Zombie Runner"
+          repo="https://github.com/kana2001/Zombie-Runner"
+          link="https://simmer.io/@kana2001/zombie-runner"
+        />
+      </div>
+    </div>
+  );
 };
