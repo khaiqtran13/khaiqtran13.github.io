451ef59b374e18749cc7744b2de5f5a7e72ff47e
Khai <khaiqtran13@gmail.com>
2023-03-01 12:24:14 -0500

    color change to orange and blue scheme for page 2

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,7 +4,7 @@ import { FaLink, FaCode } from "react-icons/fa";
 export const Projects = () => {
   const UIPill = ({ title }) => {
     return (
-      <div className="glass rounded-full text-white w-fit px-2 m-1 border-0">
+      <div className="rounded-full bg-indigo-800 text-white w-fit px-2 m-1 border-0">
         {title}
       </div>
     );
@@ -12,7 +12,7 @@ export const Projects = () => {
 
   const Card = ({ title, subtitle, link, repo, tech }) => (
     <div>
-      <div className="justify-between glass p-3 m-3 md:mx-0 md:h-[210px] h-[320px] shadow-2xl">
+      <div className="justify-between bg-indigo-800 rounded-[2rem] p-3 m-3 md:mx-0 md:h-[210px] h-[320px] shadow-2xl :glass:">
         <div className="ml-5 mr-2 grid grid-cols-6 h-full">
           <div className="flex flex-col md:col-span-5 col-span-full mr-4">
             <h3 className="mt-2 text-white text-3xl font-semibold">{title}</h3>
@@ -26,9 +26,9 @@ export const Projects = () => {
                     href={link}
                     target="_blank"
                     rel="noreferrer"
-                    className="w-fit hover:scale-150 mr-2"
+                    className="w-fit hover:scale-125 mr-2"
                   >
-                    <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-150 bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+                    <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-125 bg-orange-400">
                       <FaLink />
                     </button>
                   </a>
@@ -40,7 +40,7 @@ export const Projects = () => {
                     rel="noreferrer"
                     className="w-fit hover:scale-125"
                   >
-                    <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-150 bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
+                    <button className="w-fit p-4 rounded-lg shadow-xl hover:scale-125 bg-orange-400">
                       <FaCode />
                     </button>
                   </a>
@@ -85,9 +85,11 @@ export const Projects = () => {
   ];
 
   return (
-    <div className="flex-col bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex justify-center">
+    <div className="flex-col flex justify-center gradient">
       <div className="md:max-w-[65%] mx-auto">
-        <p className="pb-6 text-6xl mx-auto mt-4 text-center">Projects</p>
+        <p className="pb-6 text-6xl mx-auto mt-4 text-center text-red-500 font-bold font-mono tracking-[-0.10em]">
+          Projects
+        </p>
         <div className="flex flex-wrap max-w-3/4 justify-center h-fit">
           <Card
             title="CoverMe"
@@ -124,9 +126,11 @@ export const Projects = () => {
           />
         </div>
         <div className="m-3 md:mx-0">
-          <p className="pb-6 text-6xl mx-auto mt-4 text-center">Skills</p>
+          <p className="pb-6 text-6xl mx-auto mt-4 text-center text-red-500 font-bold font-mono tracking-[-0.10em]">
+            Skills
+          </p>
           <div>
-            <div className="glass2 rounded-lg p-2 my-3">
+            <div className="bg-orange-400 rounded-lg p-2 my-3">
               <div className="mx-2 flex flex-wrap">
                 <p className="font-semibold">Languages:</p>
                 <div className="flex flex-wrap">
@@ -136,7 +140,7 @@ export const Projects = () => {
                 </div>
               </div>
             </div>
-            <div className="glass2 rounded-lg p-2 my-3">
+            <div className="bg-orange-400 rounded-lg p-2 my-3">
               <div className="mx-2 flex flex-wrap">
                 <p className="font-semibold">Technologies:</p>
                 <div className="flex flex-wrap">
